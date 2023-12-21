e07f2f84f5a651a6d7e91334e76f9e66913fed58
guansk <gskzrn@126.com>
2023-12-22 00:35:15 +0800

    格式化

--- src/runUIBuilder.tsx
+++ src/runUIBuilder.tsx
@@ -4,67 +4,68 @@ import {
   IWidgetTable,
   UIBuilder,
 } from "@lark-base-open/js-sdk";
-import { UseTranslationResponse } from 'react-i18next';
+import { UseTranslationResponse } from "react-i18next";
 
-export default async function main(uiBuilder: UIBuilder, { t }: UseTranslationResponse<'translation', undefined>) {
+export default async function main(
+  uiBuilder: UIBuilder,
+  { t }: UseTranslationResponse<"translation", undefined>
+) {
   // uiBuilder.markdown(`## ${t('text_title')}`);
-  uiBuilder.markdown(t('text_description'));
+  uiBuilder.markdown(t("text_description"));
   uiBuilder.form(
-    form => ({
+    (form) => ({
       formItems: [
         // 选择数据表
-        form.tableSelect('table', {
-          label: t('select_data_table'),
-          placeholder: t('select_data_table_placeholder'),
+        form.tableSelect("table", {
+          label: t("select_data_table"),
+          placeholder: t("select_data_table_placeholder"),
         }),
 
         // 选择地理位置字段
-        form.fieldSelect('latitudeField', {
-          label: t('select_latitude_field'),
-          sourceTable: 'table',
-          placeholder: t('select_latitude_field_placeholder'),
+        form.fieldSelect("latitudeField", {
+          label: t("select_latitude_field"),
+          sourceTable: "table",
+          placeholder: t("select_latitude_field_placeholder"),
           multiple: false,
           filterByTypes: [FieldType.Location as any],
         }),
-        form.fieldSelect('longitudeField', {
-          label: t('select_longitude_field'),
-          sourceTable: 'table',
-          placeholder: t('select_longitude_field_placeholder'),
+        form.fieldSelect("longitudeField", {
+          label: t("select_longitude_field"),
+          sourceTable: "table",
+          placeholder: t("select_longitude_field_placeholder"),
           multiple: false,
           filterByTypes: [FieldType.Location as any],
         }),
 
         // 选择距离模式
-        form.select('distanceType', {
-          label: t('select_distance_type'),
+        form.select("distanceType", {
+          label: t("select_distance_type"),
           options: [
-            { label: t('direct_distance'), value: 'direct' },
-            { label: t('driving_distance'), value: 'driving' },
-            { label: t('walking_distance'), value: 'walking' },
+            { label: t("direct_distance"), value: "direct" },
+            { label: t("driving_distance"), value: "driving" },
+            { label: t("walking_distance"), value: "walking" },
           ],
-          placeholder: t('select_distance_type_placeholder'),
+          placeholder: t("select_distance_type_placeholder"),
         }),
 
         // 选择输出字段：距离
-        form.fieldSelect('outputField', {
-          label: t('select_output_field'),
-          sourceTable: 'table',
-          placeholder: t('select_output_field_placeholder'),
+        form.fieldSelect("outputField", {
+          label: t("select_output_field"),
+          sourceTable: "table",
+          placeholder: t("select_output_field_placeholder"),
           multiple: false,
           filterByTypes: [FieldType.Number as any],
         }),
         // 选择输出字段：时间
-        form.fieldSelect('outputField_duration', {
-          label: t('select_output_field_duration'),
-          sourceTable: 'table',
-          placeholder: t('select_output_field_duration_placeholder'),
+        form.fieldSelect("outputField_duration", {
+          label: t("select_output_field_duration"),
+          sourceTable: "table",
+          placeholder: t("select_output_field_duration_placeholder"),
           multiple: false,
           filterByTypes: [FieldType.Number as any],
         }),
-
-
       ],
-      buttons: [t('button')],
+      buttons: [t("button")],
     }),
     async ({ values }) => {
       // 必填
@@ -78,29 +79,33 @@ export default async function main(uiBuilder: UIBuilder, { t }: UseTranslationRe
 
       // 检查是否填写了必填字段
       if (!table || !latitudeField || !longitudeField || !distanceType) {
-        uiBuilder.message.error(t('form_incomplete_error'));
-        throw new Error(t('form_incomplete_error'));
+        uiBuilder.message.error(t("form_incomplete_error"));
+        throw new Error(t("form_incomplete_error"));
       }
 
       // 检查是否至少填写了一个输出字段
       if (!outputFieldDistance && !outputFieldDuration) {
-        uiBuilder.message.error(t('output_field_error'));
-        throw new Error(t('output_field_error'));
+        uiBuilder.message.error(t("output_field_error"));
+        throw new Error(t("output_field_error"));
       }
 
       // 检查两个输入字段是否是同一个字段
       if (latitudeField.id === longitudeField.id) {
-        uiBuilder.message.error(t('latitude_longitude_field_error'));
-        throw new Error(t('latitude_longitude_field_error'));
+        uiBuilder.message.error(t("latitude_longitude_field_error"));
+        throw new Error(t("latitude_longitude_field_error"));
       }
 
       // 检查两个输出字段是否是同一个字段
-      if (outputFieldDistance && outputFieldDuration && outputFieldDistance.id === outputFieldDuration.id) {
-        uiBuilder.message.error(t('output_field_same_error'));
-        throw new Error(t('output_field_same_error'));
+      if (
+        outputFieldDistance &&
+        outputFieldDuration &&
+        outputFieldDistance.id === outputFieldDuration.id
+      ) {
+        uiBuilder.message.error(t("output_field_same_error"));
+        throw new Error(t("output_field_same_error"));
       }
 
-      uiBuilder.showLoading(t('processing_data'));
+      uiBuilder.showLoading(t("processing_data"));
 
       // 获取纪录列表
       const latitudeRecords = await latitudeField.getFieldValueList();
@@ -113,22 +118,37 @@ export default async function main(uiBuilder: UIBuilder, { t }: UseTranslationRe
       latitudeRecords.forEach((latRecord, index) => {
         if (!longitudeRecords[index]) {
           uiBuilder.hideLoading();
-          uiBuilder.message.error(t('longitude_record_missing_error', { recordId: latRecord.record_id }));
-          throw new Error(t('longitude_record_missing_error', { recordId: latRecord.record_id }));
+          uiBuilder.message.error(
+            t("longitude_record_missing_error", {
+              recordId: latRecord.record_id,
+            })
+          );
+          throw new Error(
+            t("longitude_record_missing_error", {
+              recordId: latRecord.record_id,
+            })
+          );
         }
       });
 
       const updates = [];
       for (let index = 0; index < latitudeRecords.length; index++) {
         const latRecord = latitudeRecords[index];
-        const [lon1, lat1] = latRecord.value.location.split(',').map(Number);
-        const [lon2, lat2] = longitudeRecords[index].value.location.split(',').map(Number);
+        const [lon1, lat1] = latRecord.value.location.split(",").map(Number);
+        const [lon2, lat2] = longitudeRecords[index].value.location
+          .split(",")
+          .map(Number);
 
         const { distance, duration } = await calculateDistance(
-          lon1, lat1, lon2, lat2, distanceType, "6e1abfcb4d7681ab33ec051c0a25dfda",
+          lon1,
+          lat1,
+          lon2,
+          lat2,
+          distanceType,
+          "6e1abfcb4d7681ab33ec051c0a25dfda",
           (errorMsg) => {
             uiBuilder.hideLoading();
-            uiBuilder.message.error(t('APIerror')); // 显示错误消息
+            uiBuilder.message.error(t("APIerror")); // 显示错误消息
           }
         );
 
@@ -157,8 +177,8 @@ export default async function main(uiBuilder: UIBuilder, { t }: UseTranslationRe
       }
 
       uiBuilder.hideLoading();
-      uiBuilder.message.success(t('completed'));
-    },
+      uiBuilder.message.success(t("completed"));
+    }
   );
 }
 
@@ -185,25 +205,31 @@ export default async function main(uiBuilder: UIBuilder, { t }: UseTranslationRe
 //   return deg * (Math.PI / 180);
 // }
 
-
-async function calculateDistance(lon1, lat1, lon2, lat2, distanceType, apiKey, errorCallback) {
-
+async function calculateDistance(
+  lon1,
+  lat1,
+  lon2,
+  lat2,
+  distanceType,
+  apiKey,
+  errorCallback
+) {
   const origins = `${lon1},${lat1}`;
   const destination = `${lon2},${lat2}`;
   let type;
 
   switch (distanceType) {
-    case 'direct':
+    case "direct":
       type = 0;
       break;
-    case 'driving':
+    case "driving":
       type = 1;
       break;
-    case 'walking':
+    case "walking":
       type = 3;
       break;
     default:
-      throw new Error('Unknown distance type');
+      throw new Error("Unknown distance type");
   }
 
   const url = `https://restapi.amap.com/v3/distance?origins=${origins}&destination=${destination}&type=${type}&key=${apiKey}`;
@@ -214,15 +240,15 @@ async function calculateDistance(lon1, lat1, lon2, lat2, distanceType, apiKey, e
     const response = await fetch(url);
     const data = await response.json();
 
-    if (data.status !== '1') {
-      throw new Error('AMap API request failed');
+    if (data.status !== "1") {
+      throw new Error("AMap API request failed");
     }
     const result = data.results[0];
     // console.log("result:", result);
     // console.log("result.distance:", result.distance);
     return {
       distance: result.distance / 1000, // 转换为千米
-      duration: result.duration / 60     // 转换为分钟
+      duration: result.duration / 60, // 转换为分钟
     };
   } catch (error) {
     console.error(error);
